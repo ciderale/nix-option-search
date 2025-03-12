c73fdbfbf96efaeb6b8fb5d4cc0991540e8ee006
Alain Lehmann <alain.lehmann@gmail.com>
2025-03-12 22:00:47 +0100

    externalize nix-package-search.sh

--- nix-package-search.nix
+++ nix-package-search.nix
@@ -7,49 +7,5 @@
 writeShellApplication {
   name = "nix-package-search";
   runtimeInputs = [jq fzf coreutils];
-  text = ''
-    NIXPKGS_EXPR=''${NIXPKGS_EXPR:-nixpkgs}
-    INFO="$(cat <<EOF
-    First word of Query is sent to nix search
-
-    Additional words are filtering in fzf:
-    - ! Prefix to exclude matches
-    - ' To require exact matches
-    EOF
-    )"
-
-    NO_LEGACY_PACKAGE='def packageName: sub("[^.]*.[^.]*.";"")'
-    PAD='def pad(x;n): x + (" "*(x | n - length ))'
-    FMT='to_entries | map([pad(.value.version;10), pad(.key|packageName;15), .value.description, .key]|@tsv) | .[]'
-    LISTING="$PAD; $NO_LEGACY_PACKAGE; $FMT"
-    DETAIL='[
-        ([
-          (if (.available) then "Available" else "" end),
-          (if (.unsupported) then "Unsupported" else "" end),
-          (if (.broken) then "Broken" else "" end),
-          (if (.unfree) then "Unfree" else "" end),
-          (if (.insecure) then "Insecure" else "" end)
-        ] | join("\t")),
-        "======",
-        "Name:       \t" + .name,
-        "Homepage:   \t" + .homepage,
-        "Description:\t" + .description,
-        (if (.longDescription>"") then "====== Long Description\n"+ .longDescription else "" end),
-        "====== Platform",
-        (.platforms|sort|join(","))
-    ] | join("\n")'
-
-    (echo -e "Version  \tPackage   \tDescription  \tKey"
-     nix search "$NIXPKGS_EXPR" --json "''${1:-.}" | jq -r "$LISTING"
-    ) | fzf --exit-0 --sync \
-    --exact --reverse \
-    --preview "nix eval --json '$NIXPKGS_EXPR#{4}.meta' --json | jq -r '$DETAIL'" \
-    --delimiter '\t' --with-nth ..3 --accept-nth 2 \
-    --prompt="Nixpkgs Search (Press ? for help)> " \
-    --bind "?:preview:echo \"$INFO\"" \
-    --header-lines 1 \
-    --no-hscroll \
-    --preview-window=bottom,wrap \
-    --tiebreak=chunk,begin
-  '';
+  text = builtins.readFile ./nix-package-search.sh;
 }
