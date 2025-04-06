ef596426b1ad3f88af19aabe327a8570b32d5d4a
Alain Lehmann <alain.lehmann@gmail.com>
2025-04-06 22:39:47 +0200

    refactor standalone.nix

---
+++ jqlib/flake-metadata.jq
@@ -0,0 +1,66 @@
+include "formatting";
+
+def flakeRef:
+	if (.type == "tarball") then .url
+	else .type + ":" + .owner +"/" + .repo + "/" +.rev
+	end;
+
+def flakeRefWithHash:
+	(. | flakeRef) + "?narHash" + .narHash;
+
+def myformat:
+	{ref: . | flakeRefWithHash, lastModified: .lastModified | todateiso8601 };
+
+def asList:
+	to_entries | map(.value + {key: .key});
+
+def padding:
+	[.[0], pad(.[1];10), pad(.[2];20), pad(.[3];10)];
+
+def display: .[] | padding | @tsv;
+
+def header: [[
+  "args",
+	"Type",
+	"Name",
+	"Date",
+	"FlakeRef"
+]] | display;
+
+
+def packages:
+	["packages,"+.ref, "packages", .key, .lastModified, .ref];
+
+def detectOption:
+	if .ref | contains("nixpkgs") then
+		"nixos"
+	elif .ref | contains("home-manager") then
+		"home-manager"
+	elif .ref | contains("devenv") then
+		"devenv"
+	elif .ref | contains("kubenix") then
+		"kubenix"
+	else
+		empty
+	end;
+
+def options:
+  (. | detectOption) as $optionType | [
+	  "options,"+$optionType+","+.ref,
+		"options", $optionType, .lastModified, .ref
+  ];
+
+def generateSelection:
+	packages, options;
+
+def list:
+	.locks.nodes | map_values(select(.locked) | .locked | myformat) | asList | map(generateSelection) | display;
+
+def static: [
+    ({ref: "nixpkgs", key: "nixpkgs", lastModified: "latest"} | packages),
+    ({ref: "nixpkgs/nixpkgs-unstable", key: "nixpkgs-unstable", lastModified: "latest"}| packages),
+    ({ref: "nixpkgs/nixpkgs-unstable", key: "nixos-unstable", lastModified: "latest"}| options),
+    ({ref: "github:cachix/devenv", key: "devenv", lastModified: "latest"} | options),
+    ({ref: "github:hall/kubenix", key: "kubenix", lastModified: "latest"} | options),
+		empty
+	] | display;
--- standalone.nix
+++ standalone.nix
@@ -4,70 +4,91 @@
   nix-package-search,
   fzf,
   coreutils,
+  jq,
 }: let
   nix-discover-standalone = writeShellApplication {
     name = "nix-discover-standalone";
-    runtimeInputs = [nix-option-search-cli nix-package-search fzf coreutils];
+    runtimeInputs = [nix-option-search-cli nix-package-search fzf coreutils jq];
+    runtimeEnv.JQLIB = ./jqlib;
     text = ''
       function option_json_path() {
-        JSON_PATH=$1
-        shift
-        JSON_DRV=$(nix build --no-link --print-out-paths "''${@}")
+        ARGS=(--no-link --print-out-paths)
+        case "$TYPE" in
+          nixos)
+            ARGS+=(--impure --expr "(import (builtins.getFlake \"$FLAKE_REF\" + /nixos/release.nix) {}).options")
+            JSON_PATH="/share/doc/nixos/options.json"
+            ;;
+          home-manager)
+            ARGS+=("$FLAKE_REF#docs-json")
+            JSON_PATH="/share/doc/home-manager/options.json"
+            ;;
+          devenv)
+            ARGS+=("$FLAKE_REF#devenv-docs-options-json")
+            JSON_PATH="/share/doc/nixos/options.json"
+            ;;
+          kubenix)
+            ARGS+=("$FLAKE_REF#docs")
+            JSON_PATH=""
+            ;;
+          *)
+            echo "unknown uption type $TYPE"
+            exit 1
+            ;;
+        esac
+        JSON_DRV=$(nix build --no-link --print-out-paths "''${ARGS[@]}")
         echo "$JSON_DRV''${JSON_PATH}"
       }
 
-      if [ -z "''${OPTIONS_JSON:-}" ]; then
-        KNOWN="nixpkgs|packages:nixpkgs/nixpkgs-unstable|packages:.|devenv|home-manager|nixos|kubenix"
-        EXTRA="hm|custom"
-        case "''${1:-fzf}" in
-          nixpkgs|packages*)
-            export NIXPKGS_EXPR=''${1#*:}
-            shift
-            exec nix-package-search "''${@}"
-            ;;
+      function local_flake_refs() {
+        nix flake metadata --json | jq -L "$JQLIB" -r 'include "flake-metadata"; list' --exit-status
+      }
 
-          home-manager|hm)
-            OPTIONS_JSON=$(option_json_path "/share/doc/home-manager/options.json" "home-manager#docs-json")
-            ;;
+      function global_flake_refs() {
+        jq -L "$JQLIB" -n -r 'include "flake-metadata"; static'
+      }
 
-          nixos)
-            OPTIONS_JSON=$(option_json_path "/share/doc/nixos/options.json" --impure --expr '(import (builtins.getFlake "nixpkgs" + /nixos/release.nix) {}).options')
-            ;;
+      function search_choices() {
+        jq -L "$JQLIB" -n -r 'include "flake-metadata"; header'
+        local_flake_refs;
+        global_flake_refs
+      }
 
-          devenv)
-            OPTIONS_JSON=$(option_json_path "/share/doc/nixos/options.json" "github:cachix/devenv#devenv-docs-options-json")
-            ;;
+      case "''${1:-default}" in
+        packages)
+          echo "seach packages $*"
+          shift
+          export NIXPKGS_EXPR=''${1}
+          shift
+          exec nix-package-search
+          ;;
 
-          kubenix)
-            OPTIONS_JSON=$(option_json_path "" "github:hall/kubenix#docs");
-            ;;
+        options)
+          shift
+          TYPE=$1
+          FLAKE_REF=$2
+          shift
+          echo "search options: $TYPE in $FLAKE_REF"
+          OPTIONS_JSON=$(option_json_path "/share/doc/nixos/options.json")
+          export OPTIONS_JSON
+          nix-option-search
+          ;;
 
-          custom)
-            shift 1
-            echo "Using custom definition: $*"
-            OPTIONS_JSON=$(option_json_path "''${@}")
-            ;;
-
-          usage|--help|-h)
-            echo "Usage: $(basename "$0") [tool]"
-            echo "  no-arguments:   fzf-based selection of tool"
-            echo "  tool:           any of $KNOWN|$EXTRA"
-            echo "  custom: arg2-*: nix build argument for options.json derivation build"
-            echo "  custom: arg1  : path to options.json within the above built derivation"
-            echo "$KNOWN"
-            exit 1
-            ;;
-
-          fzf|*)
-            SELECTION="$(echo "$KNOWN" | tr '|' '\n' | fzf --no-sort --header 'What options would you like to search through?')"
-            if [ -n "$SELECTION" ]; then
-              exec $0 "$SELECTION"
-            fi
-            ;;
-        esac
-      fi
-      export OPTIONS_JSON
-      nix-option-search
+        *)
+          echo "select something using fzf"
+          CHOICE=$(
+            search_choices | fzf -e --sort \
+                --header-lines 1 \
+                --delimiter '\t' \
+                --with-nth "2.." \
+                --header 'What options would you like to search through?' \
+                --query "''${1:-}" --select-1 \
+                --accept-nth "{1}" \
+                | tr -d '\n'
+          )
+          IFS="," read -r -a CHOICE_ARR <<< "$CHOICE"
+          $0 "''${CHOICE_ARR[@]}"
+          ;;
+      esac
     '';
   };
 in {
